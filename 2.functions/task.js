64a2c095c77bc6b04b0b250fbe59f58c594a57b8
kovinant <77713840+kovinant@users.noreply.github.com>
2024-04-16 21:24:19 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,55 +1,68 @@
-function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-    return arr1.every((value, index) => value === arr2[index]);
+function summElementsWorker(...arr) {
+    if (arr.length === 0) return 0;
+    return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
-function getUsersAverageAge(users, gender) {
-    const filteredUsers = users.filter(user => user.gender === gender);
-    
-    if (filteredUsers.length === 0) {
-        return "No users found for the selected gender.";
-    }
-    
-    const averageAge = filteredUsers.map(user => user.age).reduce((acc, curr) => acc + curr, 0) / filteredUsers.length;
-    
-    return averageAge.toFixed(1); 
+function differenceMaxMinWorker(...arr) {
+    if (arr.length === 0) return 0;
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    return max - min;
 }
 
+function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) return 0;
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
 
-document.getElementById('compareArraysForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const array1 = document.getElementById('array1').value.split(',').map(Number);
-    const array2 = document.getElementById('array2').value.split(',').map(Number);
-    const result = compareArrays(array1, array2);
-    document.getElementById('compareArraysResult').textContent = `Arrays are ${result ? '' : 'not '}equal.`;
-});
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num;
+        } else {
+            sumOddElement += num;
+        }
+    }
 
+    return sumEvenElement - sumOddElement;
+}
 
-document.getElementById('usersAverageAgeForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const gender = document.getElementById('gender').value;
-    const result = getUsersAverageAge(people, gender);
-    document.getElementById('usersAgeAverage').textContent = `Average age for ${gender}: ${result}`;
-});
+function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) return 0;
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
 
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num;
+            countEvenElement++;
+        }
+    }
 
-const people = [
-    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
-    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
-    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
-    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
-    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
-    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
-    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
-    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
-    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
-    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
-    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
-    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
-    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
-    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+    return countEvenElement === 0 ? 0 : sumEvenElement / countEvenElement;
+}
+
+function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
+
+    for (let arr of arrOfArr) {
+        const result = func(...arr);
+
+        if (result > maxWorkerResult) {
+            maxWorkerResult = result;
+        }
+    }
+
+    return maxWorkerResult;
+}
+
+const arr = [
+    [10, 10, 11, 20, 10],
+    [67, 10, 2, 39, 88],
+    [72, 75, 51, 87, 43],
+    [30, 41, 55, 96, 62]
 ];
 
-
+console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
+console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
+console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
+console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
