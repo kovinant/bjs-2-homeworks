ef4999aaa5112922e05561367ccac954f9e5d455
kovinant <77713840+kovinant@users.noreply.github.com>
2024-04-17 22:08:20 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,35 +1,68 @@
-function getArrayParams(...arr) {
-    let min = arr[0];
-    let max = arr[0];
-    let sum = 0;
+function summElementsWorker(...arr) {
+    if (arr.length === 0) return 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-        sum += arr[i];
-    }
-
-    const avg = (sum / arr.length).toFixed(2);
-
-    return { min, max, avg: Number(avg) };
+    return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
-const result1 = getArrayParams(-99, 99, 10);
-const result2 = getArrayParams(1, 2, 3, -100, 10);
-const result3 = getArrayParams(5);
+function differenceMaxMinWorker(...arr) {
+    if (arr.length === 0) return 0;
 
-const resultContainer1 = document.createElement('div');
-resultContainer1.textContent = Min: ${result1.min}, Max: ${result1.max}, Avg: ${result1.avg};
-document.body.appendChild(resultContainer1);
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
 
-const resultContainer2 = document.createElement('div');
-resultContainer2.textContent = Min: ${result2.min}, Max: ${result2.max}, Avg: ${result2.avg};
-document.body.appendChild(resultContainer2);
+    return max - min;
+}
 
-const resultContainer3 = document.createElement('div');
-resultContainer3.textContent = Min: ${result3.min}, Max: ${result3.max}, Avg: ${result3.avg};
-document.body.appendChild(resultContainer3);
+function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) return 0;
+
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num;
+        } else {
+            sumOddElement += num;
+        }
+    }
+
+    return sumEvenElement - sumOddElement;
+}
+
+function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) return 0;
+
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEvenElement += num;
+            countEvenElement++;
+        }
+    }
+
+    return countEvenElement === 0 ? 0 : sumEvenElement / countEvenElement;
+}
+
+function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
+
+    for (let arr of arrOfArr) {
+        const result = func(...arr);
+
+        if (result > maxWorkerResult) {
+            maxWorkerResult = result;
+        }
+    }
+
+    return maxWorkerResult;
+}
+
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+
+console.log(makeWork(arr, summElementsWorker)); // 328
+console.log(makeWork(arr, differenceMaxMinWorker)); // 86
+console.log(makeWork(arr, differenceEvenOddWorker)); // 92
+console.log(makeWork(arr, averageEvenElementsWorker)); // 72
